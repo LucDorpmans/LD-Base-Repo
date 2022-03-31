54abb3f251ac2dd73eda2e3ae8edd1ca69b6645e
LucDorpmans <GitHub@sure.nl>
2022-03-31 23:42:11 +0200

    Upd

--- GetMyScriptsCore.ps1
+++ GetMyScriptsCore.ps1
@@ -4,6 +4,6 @@ Function Get-MyScript { Param( [string]$AFile,[switch]$EditFile = $False,
 			Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/My-55039-Repo/main/$AFile"  -Outfile "$SPath$AFile" 
 			If ($EditFile) { Notepad  ("$SPath$AFile" )} }
 		
-Get-MyScript "PowerShell-7.1.4-Core-Download+Install.ps1" 
+Get-MyScript "PowerShell-7.2.1-Core-Download+Install.ps1" 
 Get-MyScript "NuGet+PSGallery.ps1" 
 Get-MyScript "55039-Install-Modules.ps1" 
